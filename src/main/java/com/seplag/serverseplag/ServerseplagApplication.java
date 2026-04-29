21f255f087cd5a5c9d3b178fde182a22f93570ec
kevin-nivek <kevinbotelhox@outlook.com>
2026-04-29 10:34:46 -0400

    Criacao do projeto e configuracao de conexao com o banco

---
+++ src/main/java/com/seplag/serverseplag/ServerseplagApplication.java
@@ -0,0 +1,13 @@
+package com.seplag.serverseplag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerseplagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerseplagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seplag/serverseplag/ServerseplagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seplag.serverseplag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerseplagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
